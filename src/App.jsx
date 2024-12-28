72b28d665f1998deacaae3d8c8b5a7dd8f47659f
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-12-28 10:58:33 +0530

    upadte: Chain Nav UI using Daisy UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import ChainNav from './ChainNav';
 import CurrentChainAddresses from './CurrentChainAddresses';
 
 import { mnemonicToSeedSync, generateMnemonic } from '@scure/bip39';
 import { wordlist } from '@scure/bip39/wordlists/english';
 import { createWallet, getDerivedPath } from './utils';
 import Mnemonic from './Mnemonic';
+import Header from './Header';
 
 export default function App() {
   const [mnemonic, setMnemonic] = useState();
@@ -35,6 +35,10 @@ export default function App() {
   }
 
   function createAddress() {
+    if (!mnemonic) {
+      alert('Please add your mnemonic first');
+      return;
+    }
     const derivationPath = getDerivedPath(chains, chain, accountIndex[chain]);
     const wallet = createWallet(mnemonic, derivationPath, seed, chain, chains);
     setAccounts([...accounts, wallet]);
@@ -47,26 +51,19 @@ export default function App() {
   }
 
   return (
-    <div className='bg-[url("/wavey-fingerprint.svg")] min-h-screen'>
-      <h1 className='text-center bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 text-transparent bg-clip-text text-5xl md:text-7xl font-semibold py-5'>
-        Wallet Generator
-      </h1>
+    <>
+      <Header chains={chains} setChain={setChain} createAddress={createAddress} />
+
       <div className='flex flex-col bg-gradient-to-r'>
-        <button onClick={genMemonic}>
-          <div className='relative flex w-fit h-fit items-center justify-center border-4 border-purple-700 rounded-lg bg-purple-900 bg-gradient-to-tl from-slate-800 via-violet-500 to-zinc-400 bg-clip-text text-transparent px-3 py-4 mx-auto font-semibold text-xl'>
-            Generate Mnemonic
-          </div>
+        <button
+          className='relative flex w-max h-fit items-center justify-center border-4 border-purple-700 rounded-lg bg-purple-900 bg-gradient-to-tl from-slate-800 via-violet-500 to-zinc-400 bg-clip-text text-transparent p-2 mx-auto my-3 font-semibold text-xl'
+          onClick={genMemonic}
+        >
+          Generate Mnemonic
         </button>
 
         <Mnemonic mnemonic={mnemonic} />
       </div>
-      <ChainNav
-        setChain={setChain}
-        chains={chains}
-        chain={chain}
-        createAddress={createAddress}
-        mnemonic={mnemonic}
-      />
 
       <div className='text-center flex flex-col'>
         {currentChainAddresses.length !== 0 ? (
@@ -75,6 +72,6 @@ export default function App() {
           <div className='text-white my-3 text-xl'>No wallets to show</div>
         )}
       </div>
-    </div>
+    </>
   );
 }
